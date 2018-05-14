8dd1f0b5d1394c813cd1e5deb9173bd7d450a113
Jarso <jarso@grapheno.cl>
2018-05-14 16:29:38 -0400

    juegogato finalizado

---
+++ Lenguaje C/Juegogato.c
@@ -0,0 +1,129 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int ij=0, im=0, ut, bdt[9], i=0;  //ij=marcadorjugador; im:marcador maquina; ut: posición ultimo turno; bdt: movimientos guardados; i: tamaño de bdt;
+int ct=1, g;   // contador de turno y ganador
+int r=0;
+char p[9];  //posición
+
+void maquina();
+void jugador();
+void tablero();
+void evaluar();
+
+int main(){
+
+    tablero();
+
+    while((ct<=9) && (g==0)){      
+
+        
+        jugador();
+        evaluar();  
+        if(g==0){              
+        maquina();
+        evaluar();
+        }
+
+    }
+    return 0;
+}
+
+
+//**************************************************************************************************************************
+
+
+void maquina(void){
+
+    srand (time(NULL));
+    p[im];
+    while(im==bdt[0] ||  im==bdt[1] ||  im==bdt[2] || im==bdt[3] || im==bdt[4] || im==bdt[5] || im==bdt[6] || im==bdt[7]){   
+
+        im = (rand() % 9) + 1;
+        } 
+    p[im]='O';
+    bdt[i]=im;  
+    i=i+1;  
+    ct=ct+1;
+    tablero();
+}
+
+void jugador(void){
+
+    p[ij];
+    ut=ij;
+              
+   while(ij==bdt[0] ||  ij==bdt[1] ||  ij==bdt[2] || ij==bdt[3] || ij==bdt[4] || ij==bdt[5] || ij==bdt[6] || ij==bdt[7]){
+   
+        printf("ingrese la posición en el tablero \n");
+        scanf("%d", &ij);
+    
+       }
+    bdt[i]=ij;
+    p[ij]='X';
+    i=i+1;
+    ct=ct+1;
+    tablero();
+
+}
+
+
+void tablero(void){
+         system("clear");
+        //printf("valor de ut %d,   valor de ij %d valor de im %d\n",ut,ij, im);
+        printf("posiciones        Tablero\n");
+        printf("                         últimas jugadas: %d - %d - %d - %d - %d - %d - %d - %d - %d \n",bdt[0],bdt[1],bdt[2],bdt[3],bdt[4],bdt[5],bdt[6],bdt[7],bdt[8]);
+        printf(" 7│ 8 │9          %c│ %c │%c\n", p[7],p[8],p[9]);
+        printf(" 4│ 5 │6          %c│ %c │%c\n", p[4],p[5],p[6]);
+        printf(" 1│ 2 │3          %c│ %c │%c\n", p[1],p[2],p[3]);
+       
+
+    
+}
+
+
+void evaluar(void){
+
+    if((p[1]=='X' && p[2]=='X' && p[3]=='X')||(p[1]=='O' && p[2]=='O' && p[3]=='O')) {    //#1
+
+        printf("GANÓ!!!!!!!!!!!  \n");
+        g=1;
+    }
+     if((p[4]=='X' && p[5]=='X' && p[6]=='X')||(p[4]=='O' && p[5]=='O' && p[6]=='O')) {   //#2
+
+        printf("GANÓ!!!!!!!!!!!  \n");
+        g=1;
+    }
+     if((p[7]=='X' && p[8]=='X' && p[9]=='X')||(p[7]=='O' && p[8]=='O' && p[9]=='O')) {   //#3
+
+        printf("GANÓ!!!!!!!!!!!  \n");
+        g=1;
+    }
+     if((p[1]=='X' && p[4]=='X' && p[7]=='X')||(p[1]=='O' && p[4]=='O' && p[7]=='O')) {   //#4
+
+        printf("GANÓ!!!!!!!!!!!  \n");
+        g=1;
+    }
+     if((p[2]=='X' && p[5]=='X' && p[8]=='X')||(p[2]=='O' && p[5]=='O' && p[8]=='O')) {   //#5
+
+        printf("GANÓ!!!!!!!!!!!  \n");
+        g=1;
+    }
+     if((p[3]=='X' && p[6]=='X' && p[9]=='X')||(p[3]=='O' && p[6]=='O' && p[9]=='O')) {   //#6
+ 
+        printf("GANÓ!!!!!!!!!!!  \n");
+        g=1;
+    }
+     if((p[1]=='X' && p[5]=='X' && p[9]=='X')||(p[1]=='O' && p[5]=='O' && p[9]=='O')) {   //#7
+
+        printf("GANÓ!!!!!!!!!!!  \n");
+        g=1;
+    }
+     if((p[7]=='X' && p[5]=='X' && p[3]=='X')||(p[7]=='O' && p[5]=='O' && p[3]=='O')) {   //#8
+
+        printf("GANÓ!!!!!!!!!!! \n");
+        g=1;
+    }
+     
+}
